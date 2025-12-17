f5304143a0b32715d12579c9a2bb735c2b1850c8
Lou DE BAERE <lou_debaere@ens.univ-artois.fr>
2025-12-17 14:02:20 +0100

    début de la page de profil : affichage d'un bouton retour accueil, nom, mail, date de création du compte, avatar

--- resources/views/profil/show.blade.php
+++ resources/views/profil/show.blade.php
@@ -5,5 +5,17 @@
                 Retour à l'accueil
             </a>
         </div>
+
+        <div>
+            <div>
+                <img src="{{ $utilisateur->avatar }}"
+                     alt="Avatar de {{ $utilisateur->name }}">
+            </div>
+            <div>
+                <h1>Nom : {{ $utilisateur->name }}</h1>
+                <p>Email : {{ $utilisateur->email }}</p>
+                <p>Date d'inscription : {{ $utilisateur->created_at }}</p>
+            </div>
+        </div>
     </section>
 </x-layout.app>
