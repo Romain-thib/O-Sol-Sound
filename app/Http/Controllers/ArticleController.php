b8660ed61688e2fa289337c992a3166143bbf1e9
Shun Lembrez <shun_lembrez@ens.univ-artois.fr>
2025-12-17 13:24:45 +0100

    Ajout d'un controller pour les article pour incrémenter lse vue a chaque vue #10

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    /**
+     * Page d’accueil : 3 articles les plus récents
+     */
+    public function home()
+    {
+        $articles = Article::orderBy('created_at', 'desc')
+            ->take(3)
+            ->get();
+
+        return view('welcome', compact('articles'));
+    }
+
+    /**
+     * Page d’un article : détails + incrémentation des vues
+     */
+    public function show($id)
+    {
+        $article = Article::with([
+            'editeur',
+            'avis.user',
+            'likes',
+            'rythme',
+            'accessibilite',
+            'conclusion'
+        ])->findOrFail($id);
+
+        $article->increment('nb_vues');
+
+        return view('articles.show', compact('article'));
+    }
+
+    /**
+     * Liste des articles par rythme
+     */
+    public function byRythme($id)
+    {
+        $articles = Article::where('rythme_id', $id)
+            ->with('rythme')
+            ->get();
+
+        return view('articles.card-article', compact('articles'));
+    }
+}
