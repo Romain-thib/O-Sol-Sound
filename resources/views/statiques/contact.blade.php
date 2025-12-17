3b25304cbc94668b812665e993f0a5bf1d871ad0
Lucas Ternel <lucas.ternel62@gmail.com>
2025-12-17 23:55:08 +0100

    v1.5 page-contact reglage probleme form

--- resources/views/statiques/contact.blade.php
+++ resources/views/statiques/contact.blade.php
@@ -16,20 +16,20 @@
         <p class="contact-soustitle">Contactez-nous ! Notre équipe vous répond rapidement </p>
     </div>
 
-    <form method="POST" action="#" class="form-form>
+    <form method="POST" action="#" class="form-contact">
         <div class="form-group">
             <label for="name">Nom :</label>
-            <input type="text" id="name" name="name">
+            <input type="text" id="name" name="name" placeholder="Votre nom">
         </div>
 
         <div class="form-group">
             <label for="email">Email :</label>
-            <input type="email" id="email" name="email">
+            <input type="email" id="email" name="email" placeholder="Votre email">
         </div>
 
         <div class="form-group">
             <label for="message">Message :</label>
-            <textarea id="message" name="message"></textarea>
+            <textarea id="message" name="message" placeholder="Votre Message"></textarea>
         </div>
 
         <button type="submit" class="btn-submit">Envoyer</button>
